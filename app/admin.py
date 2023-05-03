0712a7d4018195369ab6b07fa78d694afc598a84
omar Y. sleem <97139249+OmarYassin22@users.noreply.github.com>
2023-05-04 02:15:44 +0300

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from .models import *
+from django.contrib.auth.admin import UserAdmin
+
 # Register your models here.
+admin.site.register(User)
 admin.site.register(patient)
 admin.site.register(doctor)
 admin.site.register(secretary)
 admin.site.register(schedule)
 admin.site.register(report_appiontment)
+
