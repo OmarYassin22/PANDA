67670977b26db97da20c9f2c596226180115b8e2
omar Y. sleem <97139249+OmarYassin22@users.noreply.github.com>
2023-05-04 02:55:13 +0300

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,8 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('',include('app.urls'))
+    path('',include('app.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
 ]
 urlpatterns+=staticfiles_urlpatterns()
