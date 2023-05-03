20841ebc6ce56ee70d828ba2a37140847dee4bca
omar Y. sleem <97139249+OmarYassin22@users.noreply.github.com>
2023-05-04 02:46:40 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.db.models.signals import post_save
 from django import forms
 from django.db import models
-from django.contrib.auth.models import User,AbstractUser
+from django.contrib.auth.models import AbstractUser
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 from django.conf import settings
